4f80ef5607803efd74439500254d0a495d47a6d4
Zheng Jiaqi <28040373+xiongking@users.noreply.github.com>
2025-11-03 03:59:01 +0800

    Create update_smartdns_domains.sh

---
+++ update_smartdns_domains.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# 【已更新】GitHub 仓库的 Raw 文件基础 URL
+BASE_URL="https://raw.githubusercontent.com/xiongking/smartdns-domainlist-updater/main"
+DEST_DIR="/etc/smartdns/domain-set"
+
+curl -sS "${BASE_URL}/oversea_domainlist.conf" -o "${DEST_DIR}/oversea_domainlist.conf"
+curl -sS "${BASE_URL}/domestic_domainlist.conf" -o "${DEST_DIR}/domestic_domainlist.conf"
+
+pkill -f '/usr/sbin/smartdns -f -c /etc/smartdns/smartdns.conf'
+
+sleep 1
+
+nohup /usr/sbin/smartdns -f -c /etc/smartdns/smartdns.conf >/dev/null 2>&1 &
